11b595b92ccfae04fc0661cfac7cb9a7f6ac3720
chrisnguyen1999 <neyugngnouc1999@gmail.com>
2022-05-07 17:09:54 +0700

    update default facility image

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,7 +4,7 @@ export const ENV = process.env.NODE_ENV;
 export const DEFAULT_AVATAR =
     'https://res.cloudinary.com/chriscloud1109/image/upload/v1651629584/media/default_gr1p4q.jpg';
 export const DEFAULT_FACILITY_IMAGE =
-    'https://res.cloudinary.com/chriscloud1109/image/upload/v1651629584/media/default_gr1p4q.jpg';
+    'https://res.cloudinary.com/chriscloud1109/image/upload/v1651918030/media/default_facility_mg3jws.jpg';
 export const DEFAULT_PAGE = 1;
 export const DEFAULT_LIMIT = 20;
 export const DEFAULT_START_PAGE = 1;
